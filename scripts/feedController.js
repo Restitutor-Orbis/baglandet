89f7b9eae6986789d1537873072f7737978383a5
vson <vson@itu.dk>
2021-06-23 14:27:50 +0200

    test feeds

--- scripts/feedController.js
+++ scripts/feedController.js
@@ -1,3 +1,5 @@
-var podcast_url = url = "https://rss.acast.com/altingetpolitik";
+var podcast_politics_url = "https://rss.acast.com/altingetpolitik";
+var podcast_EU_url       = "https://rss.acast.com/altingeteu";
 
-generateFeed(podcast_url, 10);
+generateFeed(podcast_politics_url, 10);
+generateFeed(podcast_EU_url, 10);
