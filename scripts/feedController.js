2e56ff45ebec83745e3586157ea383381aa34378
vson <vson@itu.dk>
2021-06-23 22:08:38 +0200

    add feeds

--- scripts/feedController.js
+++ scripts/feedController.js
@@ -1,8 +1,31 @@
-var podcast_politics_url  = "https://rss.acast.com/altingetpolitik";
-var podcast_EU_url        = "https://rss.acast.com/altingeteu";
+var podcast_politics_url   = "https://rss.acast.com/altingetpolitik";
+var podcast_EU_url         = "https://rss.acast.com/altingeteu";
 
-var politikken_debat_url  = "http://politiken.dk/rss/senestenyt";
+var politikken_debat_url   = "http://politiken.dk/rss/senestenyt";
 
-var jylland_politik_url   = "https://jyllands-posten.dk/politik/?service=rssfeed";
+var jylland_politik_url    = "https://jyllands-posten.dk/politik/?service=rssfeed";
+var jylland_kultur_url     = "https://jyllands-posten.dk/politik/?service=rssfeed";
 
-generateFeed(jylland_politik_url, 5);
+var dr_politik_url         = "https://www.dr.dk/nyheder/service/feeds/politik";
+
+var berlingske_politik_url = "https://www.berlingske.dk/content/23/rss";
+
+generateFeed_jyllandsposten(jylland_politik_url, 1,4);
+generateFeed_berlingske(berlingske_politik_url, 1,29299);
+generateTwitterFeed();
+
+
+//different tag colors. you need to know the tags in the xml to edit these
+function setTagColor(tag_type) {
+    switch(tag_type) {
+      case "Politik":
+        return "blue";
+        break;
+      case "Kultur":
+        return "purple";
+        break;
+      default:
+        return "black";
+        break;
+    }
+  }
--- scripts/rssParser.js
+++ scripts/rssParser.js
@@ -1,26 +1,179 @@
 
 const DOMPARSER = new DOMParser().parseFromString.bind(new DOMParser())
 
-/* Fetch URLs from JSON */
-function generateFeed(url, noOfArticles) {
-    var i = 0;
+/* Fetch the feed from given url. Only x number of items fetched
+*  Designed to work by the XML provided by Jyllandsposten.s
+*/
+function generateFeed_jyllandsposten(url, min, high) {
+    var i = 0; //index in total number of elements;
+    var j = 0; //number of inserted elements;
 
     fetch(url).then((res) => {
         res.text().then((xmlTxt) => {
           var domParser = new DOMParser()
           let feed = domParser.parseFromString(xmlTxt, 'text/xml')
           feed.querySelectorAll('item').forEach((item) => {
-             i++;
-             if(i > noOfArticles) throw error;
+            i++;
+            if(i < min) return;
+            if(i > high) throw error;
 
-             let h1 = document.createElement('h1')
-              h1.textContent = item.querySelector('title').textContent
-               document.getElementById('output').appendChild(h1)
+            j++;
 
-             let img = document.createElement('img')
-              img.src = item.querySelector('media:content').url
-               document.getElementById('output').appendChild(img)
-             })
-           }).catch((e) => {});
-    }).catch(() => console.error('Error in fetching the feed'))
+            let container = document.createElement('div');
+            container.classList.add('article-container');
+
+            if(j % 2 === 0) container.style.marginLeft   = "2%";
+            else            container.style.marginRight  = "2%";
+
+            //fetch basic information
+            var title       = item.querySelector('title').textContent;
+            var tag_type    = item.querySelector('category').textContent;
+            var description = item.querySelector('description').textContent;
+            var date        = item.querySelector('pubDate').textContent;
+            var newspaper   = "JYLLANDSPOSTEN";
+
+            var use_description = false;
+
+            //format information
+            date = date.substring(17, 22);
+
+            try { //due to difficulty in getting media:content... (image for this newspaper)
+              var image_url = item.getElementsByTagName('media:thumbnail')[0].attributes[0].nodeValue; //getting image node
+              addImage(image_url, container);
+            } catch(err) {
+              use_description = true;
+            }
+
+            addTag(tag_type, date, container);
+            addTitle(title, container); 
+
+
+            if(use_description)
+              addDescription(description, container);
+
+            addNewspaper(newspaper, container);
+
+            document.getElementById('news-section-1').appendChild(container);
+            })
+          });
+    }).catch(() => console.error('Error in loading the feed'))
+}
+
+
+function addImage(image_url, container) {
+  let img = document.createElement('img');
+  img.src = image_url;
+  container.appendChild(img);
+}
+
+function addTag(tag_type, date, container) {
+  let tag = document.createElement('p');
+  tag.textContent = tag_type;
+  tag.classList.add('tag');
+  tag.style.color = setTagColor(tag_type);
+  container.appendChild(tag);
+
+  addTime(date, container, tag);
+}
+
+function addTime(date, container, tag) {
+  let time = document.createElement('span');
+  time.textContent = date;
+  tag.appendChild(time);
+}
+
+function addTitle(title, container) {
+  let h1 = document.createElement('h1');
+  h1.textContent = title;
+  container.appendChild(h1);
+}
+
+function addDescription(description, container) {
+  let desc = document.createElement('p');
+  desc.textContent = description;
+  container.appendChild(desc);
+}
+
+function addNewspaper(newspaper, container) {
+  let newspaper_element = document.createElement('p');
+  newspaper_element.classList.add('newspaper-tag');
+  newspaper_element.textContent = newspaper;
+  container.appendChild(newspaper_element);
+}
+
+
+
+/* Fetch the feed from given url. Only x number of items fetched
+*  Designed to work by the XML provided by Berlingske.
+*/
+function generateFeed_berlingske(url, min, high) {
+  var i = 0; //index in total number of elements;
+  var j = 0; //number of inserted elements;
+
+  fetch(url).then((res) => {
+      res.text().then((xmlTxt) => {
+        var domParser = new DOMParser()
+        let feed = domParser.parseFromString(xmlTxt, 'text/xml')
+        feed.querySelectorAll('item').forEach((item) => {
+          i++;
+          if(i < min) return;
+          if(i > high) throw error;
+
+          j++;
+
+          let container = document.createElement('div');
+          container.classList.add('article-container');
+
+          if(j % 2 === 0) container.style.marginLeft   = "2%";
+          else            container.style.marginRight  = "2%";
+
+          //fetch basic information
+          var title       = item.querySelector('title').textContent;
+          var tag_type    = item.querySelector('category').textContent;
+          var description = item.querySelector('description').textContent;
+          var date        = item.querySelector('pubDate').textContent;
+          var image_url   = item.querySelector('enclosure').attributes[0].value;
+          var newspaper   = "BERLINGSKE";
+
+          var use_description = (image_url === undefined);
+
+          //format information
+          date = date.substring(17, 22);
+
+          if(!use_description) {
+            let image = document.createElement('img');
+            image.src = image_url;
+            container.appendChild(image);
+          }
+
+          let tag = document.createElement('p');
+          tag.textContent = tag_type;
+          tag.classList.add('tag');
+          tag.style.color = setTagColor(tag_type);
+          container.appendChild(tag);
+
+          let time = document.createElement('span');
+          time.textContent = date;
+          tag.appendChild(time);
+
+
+          let h1 = document.createElement('h1');
+          h1.textContent = title;
+          container.appendChild(h1);
+
+          if(use_description) {
+            let desc = document.createElement('p');
+            desc.textContent = description;
+            container.appendChild(desc);
+          }
+
+          let newspaper_element = document.createElement('p');
+          newspaper_element.classList.add('newspaper-tag');
+          newspaper_element.textContent = newspaper;
+          container.appendChild(newspaper_element);
+
+          document.getElementById('news-section-1').appendChild(container);
+          })
+        });
+  }).catch(() => console.error('Error in loading the feed'))
 }
