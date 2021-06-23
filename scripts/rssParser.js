de46d37a6a1277e4e00fd66a982d10dbaf162966
vson <vson@itu.dk>
2021-06-23 13:45:34 +0200

    fix name of selector

--- scripts/rssParser.js
+++ scripts/rssParser.js
@@ -7,11 +7,11 @@ var url = "https://rss.acast.com/altingetpolitik";
 fetch(url).then((res) => {
     res.text().then((xmlTxt) => {
       var domParser = new DOMParser()
-      let doc = domParser.parseFromString(xmlTxt, 'text/xml')
-      doc.querySelectorAll('item').forEach((item) => {
+      let feed = domParser.parseFromString(xmlTxt, 'text/xml')
+      feed.querySelectorAll('item').forEach((item) => {
          let h1 = document.createElement('h1')
           h1.textContent = item.querySelector('title').textContent
-           document.getElementById('output').appendChild(h1)
+           document.querySelector('output').appendChild(h1)
          })
        })
 }).catch(() => console.error('Error in fetching the feed'))
