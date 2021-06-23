93115fbfcc919423e7377c444eeb0c2a2f85d545
vson <vson@itu.dk>
2021-06-23 13:48:11 +0200

    add output style

--- scripts/rssParser.js
+++ scripts/rssParser.js
@@ -11,7 +11,7 @@ fetch(url).then((res) => {
       feed.querySelectorAll('item').forEach((item) => {
          let h1 = document.createElement('h1')
           h1.textContent = item.querySelector('title').textContent
-           document.querySelector('output').appendChild(h1)
+           document.getElementById('output').appendChild(h1)
          })
        })
 }).catch(() => console.error('Error in fetching the feed'))
