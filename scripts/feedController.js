418fb5ccc4103c8ded13b54c45eb46ea407bc8e5
vson <vson@itu.dk>
2021-06-23 16:11:10 +0200

    dsa

--- scripts/feedController.js
+++ scripts/feedController.js
@@ -1,5 +1,8 @@
 var podcast_politics_url = "https://rss.acast.com/altingetpolitik";
 var podcast_EU_url       = "https://rss.acast.com/altingeteu";
 
-generateFeed(podcast_politics_url, 10);
+var politikken_debat_url  = "http://politiken.dk/rss/senestedebat.rss";
+
+generateFeed(podcast_politics_url, 5);
 generateFeed(podcast_EU_url, 10);
+generateFeed(politikken_debat_url, 5);
