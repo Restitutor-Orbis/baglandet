5db1b6980338477d80698133d37aa2e025ca705e
vson <vson@itu.dk>
2021-06-23 14:08:05 +0200

    fix forEach break

--- scripts/rssParser.js
+++ scripts/rssParser.js
@@ -11,12 +11,12 @@ function generateFeed(url, noOfArticles) {
           let feed = domParser.parseFromString(xmlTxt, 'text/xml')
           feed.querySelectorAll('item').forEach((item) => {
              i++;
-             if(i > noOfArticles) break;
-             
+             if(i > noOfArticles) throw BreakException;
+
              let h1 = document.createElement('h1')
               h1.textContent = item.querySelector('title').textContent
                document.getElementById('output').appendChild(h1)
              })
-           })
+           }).catch(() => {if (e !== BreakException) throw e});
     }).catch(() => console.error('Error in fetching the feed'))
 }
