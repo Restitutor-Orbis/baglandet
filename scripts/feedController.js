ec366ac800ae221902ef58fb2aa5a9525ea57171
vson <vson@itu.dk>
2021-06-23 16:52:07 +0200

    s

--- scripts/feedController.js
+++ scripts/feedController.js
@@ -1,8 +1,10 @@
-var podcast_politics_url = "https://rss.acast.com/altingetpolitik";
-var podcast_EU_url       = "https://rss.acast.com/altingeteu";
+var podcast_politics_url  = "https://rss.acast.com/altingetpolitik";
+var podcast_EU_url        = "https://rss.acast.com/altingeteu";
 
 var politikken_debat_url  = "http://politiken.dk/rss/senestenyt";
 
+var jylland_politik_url   = "https://jyllands-posten.dk/politik/?service=rssfeed";
+
 generateFeed(podcast_politics_url, 5);
 generateFeed(podcast_EU_url, 10);
-generateFeed(politikken_debat_url, 5);
+generateFeed(jylland_politik_url, 5);
